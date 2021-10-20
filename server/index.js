3d32afd2cd73b72ddc9b82481cfd551dcddfd16c
xdeepakv <deepak.m.shrma@gmail.com>
2021-10-20 15:47:55 +0800

    added heroku config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const jsonServer = require("json-server");
 const express = require("express");
-const cors      = require('cors');
-
+const http = require("http");
+const url = require("url");
+const cors = require("cors");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -27,12 +28,24 @@ const redeemRouter = express.Router().get("/", (req, res) => {
 });
 server.use(cors());
 
+server.use("/proxy/", async (request, response) => {
+  var options = url.parse(`http://18.140.71.165:9090${request.url}`);
+  options.headers = request.headers;
+  options.method = request.method;
+  options.agent = false;
+  const connector = http.request(options, (res) =>
+    res.pipe(response, { end: true })
+  );
+  request.pipe(connector, { end: true });
+});
+
 server.use("/issue", issueRouter);
 server.use("/redeem", redeemRouter);
 
 server.use(middlewares);
 server.use(router);
 
+const PORT = process.env.PORT || 3000;
 server.listen(3000, () => {
   console.log(`JSON Server is running at http://localhost:3000`);
 });
